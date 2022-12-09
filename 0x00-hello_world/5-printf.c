37a62e806d7741b62d7380ad8f01b65e5be3b2b2
Chukwuebuka98 <emmanuelzico17@outlook.com>
2022-12-09 13:54:49 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return 0 after printing the function
+ * /
+int main(void)
+{
+	printf("With grammar, but thr outcome is a place of art, \n");
+	return (0);
+}
